bd72f1f03a9140000fe93549eb493b497311d571
Dillon Thoma <thomad@aldenv131.allegheny.edu>
2017-10-18 15:01:57 -0400

    Committing Laboratory Assignment 6

--- src/main/java/labsix/ManipulateDna.java
+++ src/main/java/labsix/ManipulateDna.java
@@ -3,13 +3,20 @@ package labsix;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Date;
+import java.util.Random;
 import java.util.Scanner;
 
+/** This class has a method that accepts user input and displays it as output.
+  * This program will be able to manipulate DNA.
+  */
 public class ManipulateDna {
 
+  /** This program manipulates DNA by reading in and displaying a string of
+    * DNA, then computing the complement and manipulates the string.
+    */
   public static void main(String[] args) {
     // display the name of the programmer and the date
-    System.out.println("Gregory M. Kapfhammer " + new Date());
+    System.out.println("Dillon Thoma " + new Date());
     // declare the starting file and scanner
     File dnaInputsFile = null;
     Scanner scanner = null;
@@ -21,17 +28,41 @@ public class ManipulateDna {
       System.out.println("Unable to locate file");
     }
     // Step One: Read in and display the chosen DNA string
-
+    String dnaString = scanner.nextLine();
+    System.out.println("Okay, I am going to manipulate the DNA string" + ": " + dnaString);
+    System.out.println();
+    System.out.println("Enter a string containing only the letters C, G, T, and A: " + dnaString);
     // Step Two: Compute the complement of the DNA String
-
+    String complementDna = dnaString.replace('a','T');
+    complementDna = complementDna.replace('t','A');
+    complementDna = complementDna.replace('c','G');
+    complementDna = complementDna.replace('g','C');
+    System.out.println("The complement of the DNA string " + dnaString + " is " + complementDna);
     // Step Three: Insert a randomly chosen DNA letter into the DNA String
-
+    String dnaOneToFour = "actg";
+    Random random = new Random();
+    char letter = dnaOneToFour.charAt(random.nextInt(4));
+    int randomInsertionIndexA = random.nextInt(dnaString.length());
+    String mutationOne = dnaString.substring(0, randomInsertionIndexA) + letter 
+        + dnaString.substring(randomInsertionIndexA);
+    System.out.println("Inserting " + letter + " at position " + randomInsertionIndexA 
+        + " gives " + mutationOne);
     // Step Four: Delete a DNA letter from a randomly chosen position in the DNA string
-
+    int randomRemovalIndex = random.nextInt(dnaString.length());
+    String mutationTwo = dnaString.substring(0, randomRemovalIndex);
+    String mutationThree = dnaString.substring(randomRemovalIndex + 1, dnaString.length());
+    System.out.println("Deleting from position " + randomRemovalIndex 
+        + " gives " + mutationTwo + mutationThree);
     // Step Five: Change a random position in the DNA String to a randomly chosen DNA letter
-
+    int randomInsertionIndexB = random.nextInt(dnaString.length());
+    String mutationFour = dnaString.substring(0, randomInsertionIndexB);
+    String mutationFive = dnaString.substring(randomInsertionIndexB + 1, dnaString.length());
+    System.out.println("Changing position " + randomInsertionIndexB + " gives " + mutationFour 
+        + letter + mutationFive);
     // Step Six: Display a final thankyou message
-
+    System.out.println();
+    System.out.println("Thank you for using the ManipulateDna program.");
+    System.out.println("Let's Go Pens!");
   }
 
 }
--- writing/reflection.md
+++ writing/reflection.md
@@ -1,4 +1,30 @@
-# Reflection by
+# Reflection by Dillon Thoma
 
-This lab required me to write a file called `ManipulateDNA`. Please review the
-Laboratory Assignment Sheet and add the required details to this paragraph.
+This lab required me to write a file called `ManipulateDNA`. The program
+involved taking a string of dna with the letters A, C, G, and T and
+manipulating it by outputting its complement, randomly inserting a letter,
+deleting a random letter, and changing a random position in the string. The
+most challenging part of the assignment was figuring out how to use the random
+class to take advantage of the random method in our program. For example, we
+had to insert a random insertion index and a random removal index in the
+program, in order to insert and change the position of a random letter from the
+program. This also required for me to mutate the string by using the random
+insertion and removal indeces so that the output would display properly.
+
+Also, in order to delete a random letter from the string, we needed to use
+another integer called random removal index. This allowed us to remove a random
+letter from the string. The problem I faced with this step was that you were
+not able to just use the minus symbol, as I initially had tried. Rather, I had
+to use the random removal index plus 1 and the dna string length as parameters
+for a mutation. What this did was told the program where I wanted to
+manipulate, and what action to perform.
+
+Conclusively, this laboratory assignment was much more challenging yet again.
+It required us to you different integer variables such as randomInsertionIndex
+and randomRemovalIndex, as well as multiple different mutations of a string of
+DNA. I also have learned how to use the random class in java thanks to this
+lab. At the conclusion of my lab, the output correctly displayed a DNA string
+that was going to be manipulated, the complement of that string, the insertion
+of a random letter, the deletion of a random letter, and the change of a random
+position in the string. I made sure to run the program multiple times to be
+sure that these mutations were truly random, which they were.
